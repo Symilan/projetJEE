a4b0eb6ca11ddb3c956bc27df31d06fa5ad9841c
virgile <virgile.petermann@gmail.com>
2020-03-12 12:45:37 +0100

    modèle et register en place

--- src/objectManager/Register.java
+++ src/objectManager/Register.java
@@ -34,5 +34,8 @@ public class Register {
         return getRegister().GameSessionsList;
     }
 
-    public static HashMap<Integer, Player> g(){};
+    public static HashMap<Integer, Player> getPlayersList()
+    {
+        return getRegister().PlayersList;
+    }
 }
