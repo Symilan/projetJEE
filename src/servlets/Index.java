a91265f169557ffeb64eb23c8d19e57d5cee20ac
virgile <virgile.petermann@gmail.com>
2020-03-13 11:19:09 +0100

    Model

--- src/servlets/Index.java
+++ src/servlets/Index.java
@@ -1,7 +1,14 @@
 package servlets;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 public class Index extends HttpServlet {
-
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        this.getServletContext().getRequestDispatcher( "/WEB-INF/index.jsp" ).forward( request, response );
+    }
 }
--- web/WEB-INF/index.jsp
+++ web/WEB-INF/index.jsp
@@ -11,6 +11,6 @@
     <title>$Title$</title>
   </head>
   <body>
-  modifié
+    modifié
   </body>
 </html>
