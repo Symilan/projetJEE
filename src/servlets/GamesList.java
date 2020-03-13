d40e6142578e82c7a57bffcc0ddbdd326871c2a6
virgile <virgile.petermann@gmail.com>
2020-03-13 12:21:04 +0100

    Model

--- src/servlets/GamesList.java
+++ src/servlets/GamesList.java
@@ -6,7 +6,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-public class Index extends HttpServlet {
+public class GamesList extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         this.getServletContext().getRequestDispatcher( "/WEB-INF/index.jsp" ).forward( req, resp );
--- web/WEB-INF/GameList.jsp
+++ web/WEB-INF/GameList.jsp
@@ -1,16 +1,16 @@
 <%--
   Created by IntelliJ IDEA.
   User: virgi
-  Date: 03/03/2020
-  Time: 17:32
+  Date: 13/03/2020
+  Time: 12:14
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
-  <head>
-    <title>Accueil</title>
-  </head>
-  <body>
-    modifié
-  </body>
+<head>
+    <title>Liste des jeux</title>
+</head>
+<body>
+
+</body>
 </html>
