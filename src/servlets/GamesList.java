57eb5749952d18b707a43ef0a982ac1a738c06b4
virgile <virgile.petermann@gmail.com>
2020-03-26 22:01:52 +0100

    Fin de l'affichage

--- src/servlets/GamesList.java
+++ src/servlets/GamesList.java
@@ -17,8 +17,9 @@ public class GamesList extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         Factory.getFactory().createGame(0,"BomberMan",0,true);
         Factory.getFactory().createGame(1,"Minecraft",0,true);
-        Factory.getFactory().createGame(2,"WeebLand",0,false);
+        Factory.getFactory().createGame(2,"WeebLand",0,true);
         System.out.println(Register.getGameMap());
+        req.setAttribute("selectedGame", Register.getActivatedGamesList().get(0));
         req.setAttribute("gameList", Register.getActivatedGamesList());
         req.setAttribute("player", new Player(0, "xX_D4rk_S4suk3_Xx", "sasuke@gmail.com", "04/12/1969", "Minecraft", 69, new Date(), true));
         this.getServletContext().getRequestDispatcher("/WEB-INF/gamesList.jsp").forward(req, resp);
